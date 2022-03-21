56457af98194ae3ca18b2fcc4daeb8f6e28e29ba
ViLDiiK <vilim.ondrej@gmail.com>
2022-03-21 09:04:15 +0100

    Done

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+import pylab as pl
+from pylab import linspace, pi, plot, sin, cos, show
 
-with open("soubor.txt", "r") as f:
-    pass
+f=open("soubor-ux.txt", "r")
+x=[]
+y=[]
+while 1:
+    radek=f.readline()
+    if radek=="":
+        break
+    cisla=radek.split()
+    x.append(float(cisla[0]))
+    y.append(float(cisla[1]))
+
+plot(x,y)
+pl.grid(True)
+pl.title("Graf hodnot ze souboru")
+
+pl.xlim(-10,10)
+pl.ylim([-0.25,1.1])
+
+pl.show()
